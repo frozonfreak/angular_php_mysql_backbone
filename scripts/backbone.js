d7926d4fff98a7564b51619d752161c765ab97c6
FrozonFreak <shazcool@gmail.com>
2014-10-24 22:41:48 +0800

    Revert dd5cb8f..61f9e04
    This rolls back to commit dd5cb8f68dabd6a1f1b9c256a98e4c3c06c956da.

--- scripts/backbone.js
+++ scripts/backbone.js
@@ -10,11 +10,7 @@ appName.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "partials/home.html",
       controller: 'appController',
     })
-    .state('link', {
-      url: "/link",
-      templateUrl: "partials/link.html",
-      controller: 'appLinkController',
-    })
+
     // For any unmatched url, redirect to /state1
      $urlRouterProvider.otherwise("/404");
 
@@ -23,69 +19,43 @@ appName.config(function($stateProvider, $urlRouterProvider) {
 //Handle all HTTP calls to server
 appName.factory('appSession', function($http){
     return {
-       	getCode: function() {
+       	updateNewTask: function(name, detail, deadLine) {
         	return $http.post('server/updateTask.php',{
-        		type		: 'getCode'
+        		type		: 'newTask',
+        		taskName	: name,
+        		taskDetail 	: detail,
+        		deadLine 	: deadLine
         	});
-        },
-        GenerateQRLink: function(){
-          return $http.post('server/updateTask.php',{
-            type    : 'generateQRLink'
-          });
-        },
-        checkCodeValidity: function(code){
-          return $http.post('server/updateTask.php',{
-            type    : 'checkCodeValidity',
-            code    : code
-          });
         }
     }
 });
 //controller
 appName.controller('appController', function($scope, appSession){
 
-	$scope.code;
-
+	$scope.taskList = [];
+	$scope.task;
+	$scope.taskDetail;
+	$scope.deadLine;
+	$scope.alerts = [];
+	$scope.searchText;
+	
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
     };
     $scope.updateTasks= function(data, status){
-        if(data["status"] == 1)
-          $scope.code = data["message"];
-    };
-    $scope.updateAftCodeCheck = function(data, status){
-        console.log(data);
+        console.log("Success");
     };
     $scope.displayError = function(data, status){
         console.log("Error");
     };
-    $scope.checkCodeValidity = function(){
-      appSession.checkCodeValidity($scope.code).success($scope.updateAftCodeCheck).error($scope.displayError);
-    };
-
   	//Initializer
 	init();
 	function init(){
 		
-		appSession.getCode().success($scope.updateTasks).error($scope.displayError);
+		appSession.updateNewTask().success($scope.updateTasks).error($scope.displayError);
 	};
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
 	
-});
-
-appName.controller('appLinkController', function($scope, appSession){
-  $scope.linkURL;
-  $scope.updtAfterGenerateCode= function(data, status){
-    if(data["status"] == 1)
-      $scope.linkURL = data["message"];
-  };
-  $scope.displayError = function(data, status){
-    console.log("Error");
-  };
-  init();
-  function init(){
-    appSession.GenerateQRLink().success($scope.updtAfterGenerateCode).error($scope.displayError);
-  };
 });
--- server/updateTask.php
+++ server/updateTask.php
@@ -4,46 +4,33 @@
 	$data = file_get_contents("php://input");
 	$receivedData = json_decode($data);
 
-	include_once 'vendor/GoogleAuthenticator.php';
-
-	$ga = new GoogleAuthenticator();
-	//$secret = $ga->createSecret();
-	$secret = 'RSQFDVEXMWWBIYFP'; //Needs to be hardcoded. 
+	include_once 'db_function.php';
+	$db = new DB_Functions();
 
 	if(isset($receivedData->{"type"})){
 		$response = '';
 		switch ($receivedData->{"type"}) {
-		    case 'getCode':
-				$oneCode = $ga->getCode($secret);	        
-				$response = array("status" => 1,
-		        	               "message"=> $oneCode);
+		    case 'newTask':
+		        if(isset($receivedData->{"taskName"})){
+		        	$taskName 	= $receivedData->{"taskName"};
+		        	$taskDetail = $receivedData->{"taskDetail"};
+		        	$taskDeadLine = $receivedData->{"deadLine"};
+		        	$res = $db->storeTaskDetails($taskName, $taskDetail, $taskDeadLine);
+
+		        	if($res)
+		        	    $response = array("status" => 1,
+		        	                      "message"=> "Success");
+		        	else
+		        	    $response = array("status" => 0,
+		        	                      "message"=> "Error updating to DB");
+		        }
+		        else{
+		        	$response = array("status" => 0,
+	                      "message"=> "All fields needs to be set");
+		        }
 		        echo json_encode($response);
 		    break;
-		    case 'generateQRLink':
-		    	$qrCodeUrl = $ga->getQRCodeGoogleUrl('Testing URL', $secret);
-
-		    	$response = array("status" => 1,
-		        	               "message"=> $qrCodeUrl);
-		    	echo json_encode($response);
-		    break;
-		    case 'checkCodeValidity':
-		    	if(isset($receivedData->{"code"})){
-		    		$code = $receivedData->{"code"};
-		    		$checkResult = $ga->verifyCode($secret, $code, 2);    // 2 = 2*30sec clock tolerance
-		    		if ($checkResult) {
-		    		    $response = array("status" => 1,
-		        	               "message"=> "Success");
-		    		} else {
-		    		    $response = array("status" => 0,
-		        	               "message"=> "Failed");
-		    		}
-		    	}
-		    	else{
-		    		$response = array("status" => 0,
-		        	               "message"=> "Update all parameters");
-		    	}
-		    	echo json_encode($response);
-		    break;
+		    
 		}
 	}
 	else {
